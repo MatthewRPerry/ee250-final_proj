70f7cbc4b6578aa0e63d32d4373071180e6c31f7
matthew-perry6 <44564474+matthew-perry6@users.noreply.github.com>
2020-11-08 17:38:55 -0800

    fixed tab

--- rpi_pub.py
+++ rpi_pub.py
@@ -13,28 +13,6 @@ import grovepi
 def on_connect(client, userdata, flags, rc):
     print("Connected to server (i.e., broker) with result code "+ str(rc))
 
-    #subscribe to topics of interest here
-    client.subscribe("perrymat/led")
-    client.message_callback_add("perrymat/led", led_callback)
-
-    #subscribe to lcd
-    client.subscribe("perrymat/lcd")
-    client.message_callback_add("perrymat/lcd", lcd_callback)
-
-#led callback function
-def led_callback(client, userdata, message):
-    message = str(message.payload, "utf-8")
-    if message == "LED_ON":
-        grovepi.digitalWrite(2, 1)
-    elif message == "LED_OFF":
-        grovepi.digitalWrite(2, 0)
-
-#lcd buttons callback
-def lcd_callback(client, userdata, message):
-    letter = str(message.payload, "utf-8")
-    if letter == 'a' or letter == 's' or letter =='d' or letter == 'w':
-        setText_norefresh(letter)
-
 #Default message callback. Please use custom callbacks.
 def on_message(client, userdata, msg):
     print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
@@ -57,13 +35,13 @@ if __name__ == '__main__':
     sensor_port = 2
     sensor_type = 0
 
-    #MQTT client/server setup
-    client = mqtt.Client()
-    client.on_message = on_message
-    client.on_connect = on_connect
-    #TODO: host ip and port
-    client.connect(host="127.0.0.1", port=3000, keepalive=60)
-    client.loop_start()
+    # #MQTT client/server setup
+    # client = mqtt.Client()
+    # client.on_message = on_message
+    # client.on_connect = on_connect
+    # #TODO: host ip and port
+    # client.connect(host="127.0.0.1", port=3000, keepalive=60)
+    # client.loop_start()
 
     temps = []
     hums = []
@@ -88,6 +66,6 @@ if __name__ == '__main__':
             time.sleep(1)
         
         except KeyboardInterrupt:
-		    print("Shutting down sensor")
-		    break
+            print("Shutting down sensor")
+            break
             
